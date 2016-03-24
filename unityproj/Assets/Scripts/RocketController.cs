8832d60c37c319bc2566c78efe95cff7b2e17cb1
lumpn <github@lumpn.de>
2016-03-24 23:50:52 +0100

    Initial prototype

---
+++ unityproj/Assets/Scripts/RocketController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float thrustScalar = 10f;
+    public float steerScalar = 1f;
+
+    public float inputThrust = 0f;
+    public float inputSteer = 0f;
+
+    public Vector3 thrust;
+    public Vector3 steer;
+
+    void Update ()
+    {
+        inputThrust = Input.GetAxis ("Thrust");
+        inputSteer = Input.GetAxis ("Steer");
+    }
+
+    void FixedUpdate ()
+    {
+        var fwd = transform.forward;
+        var up = transform.up;
+        thrust = fwd * (inputThrust * thrustScalar);
+        steer = up * (inputSteer * steerScalar);
+
+        rb.AddForce (thrust * Time.fixedDeltaTime);
+        rb.AddTorque (steer * Time.fixedDeltaTime);
+    }
+}

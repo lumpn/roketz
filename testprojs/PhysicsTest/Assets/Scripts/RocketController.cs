6c32fa6515a36809c5401142bf70f9c929259f88
lumpn <github@lumpn.de>
2016-04-17 02:28:20 +0200

    Tinkering with controls.

--- testprojs/PhysicsTest/Assets/Scripts/RocketController.cs
+++ testprojs/PhysicsTest/Assets/Scripts/RocketController.cs
@@ -13,6 +13,17 @@ public class RocketController : MonoBehaviour
     public Vector3 thrust;
     public Vector3 steer;
 
+    public float maxAngularVelocity = 10;
+    public float maxAngularAcceleration = 10;
+
+    public Vector3 targetAngularVelocity;
+    public Vector3 targetAngularAcceleration;
+
+    void Start ()
+    {
+        rb.maxAngularVelocity = 160;
+    }
+
     void Update ()
     {
         inputThrust = Input.GetAxis ("Thrust");
@@ -21,12 +32,16 @@ public class RocketController : MonoBehaviour
 
     void FixedUpdate ()
     {
-        var fwd = transform.forward;
-        var up = transform.up;
-        thrust = fwd * (inputThrust * thrustScalar);
-        steer = up * (inputSteer * steerScalar);
-
+        thrust = transform.forward * (inputThrust * thrustScalar);
         rb.AddForce (thrust * Time.fixedDeltaTime);
-        rb.AddTorque (steer * Time.fixedDeltaTime);
+
+        var steering = Mathf.Clamp (inputSteer * steerScalar, -maxAngularVelocity, maxAngularVelocity);
+        targetAngularVelocity = Vector3.forward * steering;
+
+        var deltaAngularVelocity = targetAngularVelocity - rb.angularVelocity;
+        targetAngularAcceleration = deltaAngularVelocity / Time.fixedDeltaTime;
+        var appliedAngularAcceleration = Vector3.ClampMagnitude (targetAngularAcceleration, maxAngularAcceleration);
+
+        rb.AddTorque (appliedAngularAcceleration, ForceMode.Acceleration);
     }
 }

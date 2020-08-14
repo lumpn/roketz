a81773cfcd7a34500bf3ba51923767dc596c652f
Jonas Boetel <lumpn@users.noreply.github.com>
2020-08-14 22:32:08 +0800

    Unity 2019.4

--- testprojs/PhysicsTest/Assets/Scripts/Physics/Editor/ManipulatorEditor.cs
+++ testprojs/PhysicsTest/Assets/Scripts/Physics/Editor/ManipulatorEditor.cs
@@ -10,18 +10,20 @@ public class ManipulatorEditor : Editor
     private Vector3 angularVelocity;
     private Vector3 angularAcceleration;
 
+    private Transform aimTarget;
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
         var manipulator = (Manipulator)target;
+        var rb = manipulator.Rigidbody;
 
         GUILayout.BeginHorizontal();
         velocity = EditorGUILayout.Vector3Field("Velocity", velocity);
         if (GUILayout.Button("Set", GUILayout.Width(80)))
         {
-            manipulator.rb.velocity = velocity;
+            rb.velocity = velocity;
         }
         GUILayout.EndHorizontal();
 
@@ -29,7 +31,7 @@ public class ManipulatorEditor : Editor
         acceleration = EditorGUILayout.Vector3Field("Acceleration", acceleration);
         if (GUILayout.Button("Apply", GUILayout.Width(80)))
         {
-            manipulator.rb.AddForce(acceleration, ForceMode.Acceleration);
+            rb.AddForce(acceleration, ForceMode.Acceleration);
         }
         GUILayout.EndHorizontal();
 
@@ -37,7 +39,7 @@ public class ManipulatorEditor : Editor
         angularVelocity = EditorGUILayout.Vector3Field("Angular velocity", angularVelocity);
         if (GUILayout.Button("Set", GUILayout.Width(80)))
         {
-            manipulator.rb.angularVelocity = angularVelocity;
+            rb.angularVelocity = angularVelocity;
         }
         GUILayout.EndHorizontal();
 
@@ -45,19 +47,29 @@ public class ManipulatorEditor : Editor
         angularAcceleration = EditorGUILayout.Vector3Field("Angular acceleration", angularAcceleration);
         if (GUILayout.Button("Apply", GUILayout.Width(80)))
         {
-            manipulator.rb.AddTorque(angularAcceleration, ForceMode.Acceleration);
+            rb.AddTorque(angularAcceleration, ForceMode.Acceleration);
         }
         GUILayout.EndHorizontal();
 
+        aimTarget = (Transform)EditorGUILayout.ObjectField("Target", aimTarget, typeof(Transform), true);
+        if (aimTarget)
+        {
+            manipulator.TargetOrientation = aimTarget.position.normalized;
+        }
+
         GUILayout.BeginVertical(GUI.skin.box);
-        EditorGUILayout.FloatField("Max Angular Velocity", manipulator.rb.maxAngularVelocity);
-        EditorGUILayout.Vector3Field("Center of mass (LS)", manipulator.rb.centerOfMass);
-        EditorGUILayout.Vector3Field("Center of mass (WS)", manipulator.rb.worldCenterOfMass);
-        EditorGUILayout.Vector3Field("Velocity", manipulator.rb.velocity);
-        EditorGUILayout.Vector3Field("Full stop acceleration", -manipulator.rb.velocity / Time.fixedDeltaTime);
-        EditorGUILayout.Vector3Field("Angular velocity", manipulator.rb.angularVelocity);
-        EditorGUILayout.Vector3Field("Full stop acceleration", -manipulator.rb.angularVelocity / Time.fixedDeltaTime);
-        EditorGUILayout.Vector3Field("Applied angular acceleration", manipulator.appliedAngularVelocity);
+        EditorGUILayout.FloatField("Max Angular Velocity", rb.maxAngularVelocity);
+        EditorGUILayout.Vector3Field("Center of mass (LS)", rb.centerOfMass);
+        EditorGUILayout.Vector3Field("Center of mass (WS)", rb.worldCenterOfMass);
+        EditorGUILayout.Vector3Field("Velocity", rb.velocity);
+        EditorGUILayout.Vector3Field("Full stop acceleration", -rb.velocity / Time.fixedDeltaTime);
+        EditorGUILayout.Vector3Field("Angular velocity", rb.angularVelocity);
+        EditorGUILayout.Vector3Field("Full stop acceleration", -rb.angularVelocity / Time.fixedDeltaTime);
+        EditorGUILayout.Separator();
+        EditorGUILayout.Vector3Field("Target angular acceleration", manipulator.TargetAngularVelocity);
+        EditorGUILayout.Vector3Field("Applied angular acceleration", manipulator.AppliedAngularVelocity);
         GUILayout.EndVertical();
+
+        Repaint();
     }
 }
--- testprojs/PhysicsTest/Assets/Scripts/Physics/Manipulator.cs
+++ testprojs/PhysicsTest/Assets/Scripts/Physics/Manipulator.cs
@@ -3,26 +3,31 @@ using System.Collections;
 
 public class Manipulator : MonoBehaviour
 {
-    public Rigidbody rb;
-
-    public Vector3 targetAngularVelocity;
+    [SerializeField] private Rigidbody rb;
 
     /// NOTE Jonas: Twice maximum angular velocity times 50 is enough to fully accelerate
     /// from -maxAngVel to +maxAngVel in one frame (50ms). 700 would be enough to cover
     /// the default maxAngVel of 7 radians per second.
-    [Range(0, 16000)] // E
-    public float maxAngularAcceleration;
-    public bool controlAngularVelocity;
+    [Range(0, 16000)]
+    [SerializeField] private float maxAngularAcceleration;
+    [SerializeField] private bool controlAngularVelocity;
 
     /// NOTE Jonas: Unity does not support angular velocity above 7 radians per second by default.
     /// Use rigibody.maxAngularVelocity to change the limit. The maximum sensible angular velocity is 160.
     /// It allows for rotations to any orientation within one frame (50ms).
     [Range(0, 160)]
-    public float maxAngularVelocity;
-    public Vector3 targetOrientation = Vector3.forward;
-    public bool controlOrientation;
+    [SerializeField] private float maxAngularVelocity;
+    [SerializeField] private Vector3 targetOrientation = Vector3.forward;
+    [SerializeField] private bool controlOrientation;
 
-    public Vector3 appliedAngularVelocity;
+    // debug
+    private Vector3 targetAngularVelocity;
+    private Vector3 appliedAngularVelocity;
+
+    public Rigidbody Rigidbody { get { return rb; } }
+    public Vector3 TargetAngularVelocity { get { return targetAngularVelocity; } }
+    public Vector3 AppliedAngularVelocity { get { return appliedAngularVelocity; } }
+    public Vector3 TargetOrientation { set { targetOrientation = value; } }
 
     void Start()
     {
@@ -49,7 +54,7 @@ public class Manipulator : MonoBehaviour
             var stopAngAcc = -curAngVel / Time.fixedDeltaTime;
             var tarAngVel = targetAngularVelocity;
             var accAngAcc = tarAngVel / Time.fixedDeltaTime;
-            var angAcc = stopAngAcc + accAngAcc;
+            var angAcc = stopAngAcc + accAngAcc; // is this correct?
 
             appliedAngularVelocity = Vector3.ClampMagnitude(angAcc, maxAngularAcceleration);
 

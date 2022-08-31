1f98dd292234301fdad13677a9342774b5842755
lumpn <lumpn@users.noreply.github.com>
2022-08-31 17:35:03 +0000

    Fixed rotation lerping

--- TestProjects/BallBalancing/Assets/Scripts/BalancingAgent.cs
+++ TestProjects/BallBalancing/Assets/Scripts/BalancingAgent.cs
@@ -3,16 +3,17 @@ using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class BalancingAgent : Agent
+public sealed class BalancingAgent : Agent
 {
     [SerializeField] private Transform initialBallPosition;
     [SerializeField] private Rigidbody ball;
+    [SerializeField] private float maxVelocity;
     [SerializeField] private float maxRotationDelta;
 
     public override void OnEpisodeBegin()
     {
         ball.position = initialBallPosition.position;
-        ball.velocity = Vector3.zero; //Random.insideUnitSphere * 0.1f;
+        ball.velocity = Random.insideUnitSphere * maxVelocity;
         transform.localRotation = Quaternion.identity;
     }
 
@@ -25,11 +26,12 @@ public class BalancingAgent : Agent
     public override void OnActionReceived(ActionBuffers actions)
     {
         var control = new Vector3(actions.ContinuousActions[0], 0, actions.ContinuousActions[1]);
-        var desiredAngles = control * 90;
+        var desiredAngles = control * 30;
         var currentAngles = transform.localRotation.eulerAngles;
-        var angles = Vector3.MoveTowards(currentAngles, desiredAngles, maxRotationDelta);
+        var angleX = Mathf.MoveTowardsAngle(currentAngles.x, desiredAngles.x, maxRotationDelta);
+        var angleZ = Mathf.MoveTowardsAngle(currentAngles.z, desiredAngles.z, maxRotationDelta);
 
-        transform.localRotation = Quaternion.Euler(angles);
+        transform.localRotation = Quaternion.Euler(angleX, 0, angleZ);
 
         AddReward(0.1f);
 
@@ -37,5 +39,10 @@ public class BalancingAgent : Agent
         {
             EndEpisode();
         }
+    }
+
+    protected void FixedUpdate()
+    {
+        RequestDecision();
     }
 }

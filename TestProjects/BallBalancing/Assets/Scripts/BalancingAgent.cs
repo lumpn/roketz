2c07cf2f3a6f656559dd9a57465b37cb5c2ae17d
lumpn <lumpn@users.noreply.github.com>
2022-08-31 16:54:34 +0000

    Balancing in 3D

--- TestProjects/BallBalancing/Assets/Scripts/BalancingAgent.cs
+++ TestProjects/BallBalancing/Assets/Scripts/BalancingAgent.cs
@@ -7,31 +7,30 @@ public class BalancingAgent : Agent
 {
     [SerializeField] private Transform initialBallPosition;
     [SerializeField] private Rigidbody ball;
+    [SerializeField] private float maxRotationDelta;
 
     public override void OnEpisodeBegin()
     {
         ball.position = initialBallPosition.position;
-        ball.velocity = new Vector3(Random.Range(-2, 2), 0, 0);
+        ball.velocity = Vector3.zero; //Random.insideUnitSphere * 0.1f;
         transform.localRotation = Quaternion.identity;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(ball.position.x - transform.position.x);
-        sensor.AddObservation(ball.velocity.x);
-
-        //Debug.LogFormat("Sending angle {0}, pos {1}, vel {2}", transform.localRotation.eulerAngles.z, ball.position.x - transform.position.x, ball.velocity.x);
+        sensor.AddObservation(ball.position - transform.position);
+        sensor.AddObservation(ball.velocity);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        var desiredAngleZ = 90 * actions.ContinuousActions[0];
-        var oldAngleZ = transform.localRotation.eulerAngles.z;
-        var newAngleZ = Mathf.MoveTowardsAngle(oldAngleZ, desiredAngleZ, 1f);
+        var control = new Vector3(actions.ContinuousActions[0], 0, actions.ContinuousActions[1]);
+        var desiredAngles = control * 90;
+        var currentAngles = transform.localRotation.eulerAngles;
+        var angles = Vector3.MoveTowards(currentAngles, desiredAngles, maxRotationDelta);
 
-        transform.localRotation = Quaternion.Euler(0, 0, newAngleZ);
+        transform.localRotation = Quaternion.Euler(angles);
 
-        //Debug.LogFormat("Received angle {0}", angleZ);
         AddReward(0.1f);
 
         if (ball.position.y < 0)
